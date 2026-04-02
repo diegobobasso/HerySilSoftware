67aba066501fa5474376d422225fe27f5f9f801a
diegobobasso <88977281+diegobobasso@users.noreply.github.com>
2026-04-02 17:17:13 -0300

    resolviendo configuracion  vercel

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<template>
+  <router-view />
+</template>
+
 <script setup>
 import { useHead } from '@vueuse/head'
 
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src',
-      '@assets': '/src/assets',
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
     },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 })
