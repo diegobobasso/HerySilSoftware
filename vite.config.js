f0642cde935526d11a2128fa6fd8e3a547181fcc
diegobobasso <88977281+diegobobasso@users.noreply.github.com>
2026-04-02 18:00:27 -0300

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    headers: {
-      'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
-    },
-  },
-  // Opcional: Redirección en el build (para SSR o pre-render)
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Asegúrate de que esto esté configurado
     },
   },
 })
